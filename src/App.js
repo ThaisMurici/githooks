a459e3f5c2d55db2e08ca3f921d3fddb8a235aea
Thais Moraes <murici.thais@gmail.com>
2019-12-24 16:54:01 -0300

    Adding repositories with 'useState' hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: "repo-1" },
+    { id: 1, name: "repo-2" },
+    { id: 1, name: "repo-3" }
+  ]);
+
+  function handleAddRepository() {
+    setRepositories([
+      ...repositories,
+      { id: Math.random(), name: "Novo repo" }
+    ]);
+  }
+
+  return (
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>Adicionar repositório</button>
+    </>
+  );
 }
 
 export default App;
